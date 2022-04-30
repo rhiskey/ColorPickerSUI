894965f0ce43ce47f28efa1d9445e5f38863c0bb
rhiskey <first3395@gmail.com>
2022-04-30 19:27:01 +0300

    Initial Commit

---
+++ ColorPickerSUI/ColorPickerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSUIApp.swift
+//  ColorPickerSUI
+//
+//  Created by Владимир Киселев on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
